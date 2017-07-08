908bcfd9139f9585753cfc8747caa988586a27ed
Ma Ngan Giang <ngangiang2103@gmail.com>
2017-07-08 22:32:48 +0800

    This is the “完成 todo list” homework

---
+++ script.js
@@ -0,0 +1,45 @@
+var App = (function () {
+
+    function _bindEvent() {
+        console.log('bind event');
+        $('#btn-addTask').on('click', _handleAddTask);
+        $('#taskContainer').on('click', '.taskItem .btn-delTask', _handleDelTask);
+    }
+
+    function _handleAddTask() {
+        console.log('button click');
+        var taskName = $('#input-taskName').val();
+        var taskDesc = $('#input-taskDesc').val();
+        $('#taskContainer').append(`
+			<div class="col-sm-4 taskItem">
+				<div class="card">
+					<div class="card-block">
+						<button type="button" class="close btn-delTask" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<h3 class="card-title">${taskName}</h3>
+						<p class="card-text">${taskDesc}</p>
+					</div>
+				</div>
+			</div>
+		`);
+        //$('#taskContainer').append('<h1>' + ${taskName} '</h1>');
+        $('#input-taskName').val('');
+        $('#input-taskDesc').val('');
+    }
+
+    function _handleDelTask() {
+        console.log('del');
+        $(this).parents('.taskItem').remove();
+    }
+
+    function init() {
+        console.log('init');
+        _bindEvent();
+    }
+
+    return {
+        init
+    }
+
+})();
